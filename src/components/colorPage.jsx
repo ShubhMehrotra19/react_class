7b9aea09632bf8870a9941fa05dfa5592b95e4cb
Stoic Coder <shubhmehrotra2022@vitbhopal.ac.in>
2024-12-16 22:49:36 +0530

    fix: made the code effecient

--- src/components/colorPage.jsx
+++ src/components/colorPage.jsx
@@ -1,36 +1,44 @@
-/* eslint-disable no-unused-vars */
+import { useState } from "react"
 
-import React, { useState } from "react"
+const colors = [
+    {
+        color: "#0099e5",
+        colorName: "blue"
+    },
+    {
+        color: "#ff4c4c",
+        colorName: "red"
+    },
+    {
+        color: "#34bf49",
+        colorName: "green"
+    },
+    {
+        color: "#ffdd00",
+        colorName: "yellow"
+    }
+]
 
 function ColorPage() {
-
     const [isColor, setIsColor] = useState("#ffffff");
 
-    function handleColor1() {
-        setIsColor("#0099e5");
-    }
-
-    function handleColor2() {
-        setIsColor("#ff4c4c")
-    }
-
-    function handleColor3() {
-        setIsColor("#34bf49")
-    }
-
-    function handleColor4() {
-        setIsColor("#ffdd00")
-    }
-
     return (
-        <div className={`h-screen w-screen bg-[${isColor}] flex justify-center items-end`}>
-      <div className="border-2 border-black bg-white rounded-md w-1/2 h-[50px] mb-10 p-2 flex justify-evenly items-center">
-                <div onClick={handleColor1} className={`bg-[#0099e5] text-white text-xl px-5 py-1 rounded-sm  cursor-pointer hover:scale-[102%] active:scale-95 transition-transform duration-300 ease-in-out`}>blue</div>
-                <div onClick={handleColor2} className={`bg-[#ff4c4c] text-white text-xl px-5 py-1 rounded-sm  cursor-pointer hover:scale-[102%] active:scale-95 transition-transform duration-300 ease-in-out`}>red</div>
-                <div onClick={handleColor3} className={`bg-[#34bf49] text-white text-xl px-5 py-1 rounded-sm  cursor-pointer hover:scale-[102%] active:scale-95 transition-transform duration-300 ease-in-out`}>green</div>
-                <div onClick={handleColor4} className={`bg-[#ffdd00] text-white text-xl px-5 py-1 rounded-sm  cursor-pointer hover:scale-[102%] active:scale-95 transition-transform duration-300 ease-in-out`}>yellow</div>
-      </div>
-    </div>
+        <div style={{ backgroundColor: isColor }} className="h-screen w-screen flex justify-center items-end">
+            <div className="border-2 border-black bg-white rounded-md w-1/2 h-[50px] mb-10 p-2 flex justify-evenly items-center">
+                {
+                    colors.map((index) => (
+                        <div 
+                            key={index.colorName}
+                            onClick={() => {setIsColor(index.color)}} 
+                            style={{ backgroundColor: index.color }}
+                            className="text-white text-xl px-5 py-1 rounded-sm cursor-pointer hover:scale-[102%] active:scale-95 transition-transform duration-300 ease-in-out"
+                        >
+                            {index.colorName}
+                        </div>
+                    ))
+                }
+            </div>
+        </div>
     )
 }
 
